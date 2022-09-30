99164cbe26c35a3ba851807f468fa52fcc0966de
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-09-30 13:00:38 -0700

    comment

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// function for the button to start 
+
+// timer starts the timer 
+    // reset timer  
+
+//gamefunction () {}
+    // (word list with array or sth else).split('')
+    // key press 
+        // if key press match with the word 
+            // shows the word 
+        // else 
+            // nothing nownowee
+    // if all the words are shown 
+        // win the game 
+        // win ++
+    // else if (timer === 0)
+        // lose 
+    // else 
+        //lose ++
+
+// press the button (main)
+    // start the game and the timer 
+    // timer funcion timer () 
+    // start the game funciton gamebox () 
+
+// local store the win and loss 
+    // reset the local store 
+
+
